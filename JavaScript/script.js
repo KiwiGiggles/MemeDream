b9fc867da9fa389d754f7f0485f37266ea304340
Alexander Öhman <f.alexander.ohman@gmail.com>
2019-02-26 17:26:48 +0100

    Navbar progress

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+//$( document ).ready(function() {
+//    var pageURL = $(location).attr("href");
+//    if(pageURL === "http://tor.skelamp.se/aleohm-7/w/MemeDream/index.php"){
+//        $('#prevBtn').addClass("disabled")
+//        $('#indexBtn').addClass("active")
+//    }
+//    
+//});
