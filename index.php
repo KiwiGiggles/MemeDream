7121fbeea3738415187061a976ba81196c1a270f
Alexander Öhman <f.alexander.ohman@gmail.com>
2019-03-26 14:52:17 +0100

    client sided voting experiments

--- index.php
+++ index.php
@@ -95,9 +95,9 @@
                 <button type="button" class="btn btn-secondary post-footer-btn">Share <span class="fas fa-share"></span></button>
                 <button type="button" class="btn btn-secondary post-footer-btn">Save <span class="far fa-bookmark"></span></button>
                 <button type="button" class="btn btn-secondary post-footer-btn">Report <span class="fas fa-flag"></span></button>
-                <button type="button" class="btn btn-secondary post-footer-btn"><span class="fas fa-arrow-up" data-fa-transform="grow-6"></span></button>
-                <button type="button" class="btn btn-secondary post-footer-btn">0</span></button>
-                <button type="button" class="btn btn-secondary post-footer-btn"><span class="fas fa-arrow-down" data-fa-transform="grow-6"></span></button>
+                <button type="button" class="btn btn-secondary post-footer-btn vote-btn" id="upVoteBtn"><i class="fas fa-arrow-up" data-fa-transform="grow-6"></i></button>
+                <input type="button" class="btn btn-secondary post-footer-btn" id="rating" value="0">
+                <button type="button" class="btn btn-secondary post-footer-btn vote-btn" id="downVoteBtn"><i class="fas fa-arrow-down" data-fa-transform="grow-6"></i></button>
               </div>
             </div>
           </div>
@@ -122,7 +122,7 @@
     </script>
 
  
-    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" data-auto-replace-svg="nest"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
