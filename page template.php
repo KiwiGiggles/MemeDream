91a1e47f8c7b13301801fa9f9b72b653ec6059f2
Alexander Öhman <f.alexander.ohman@gmail.com>
2019-04-09 09:51:36 +0200

    Refactoring

--- page template.php
+++ page template.php
@@ -15,56 +15,16 @@
 
     <div class="d-flex" id="wrapper">
 
-      <!-- Sidebar -->
-      <div class="bg-dark border-right" style="border-right: 1px solid rgba(0,0,0,.125)!important;" id="sidebar-wrapper">
-        <div class="sidebar-heading" style="color: white;">Followed users</div>
-        <div class="list-group list-group-flush">
-          <p style="color: white; margin: 5px;">You need to <a href="http://tor.skelamp.se/aleohm-7/w/MemeDream/login.php">login</a> to see followed users</p>
-          <p style="color: white; margin: 5px;">Don't have an account? <a href="http://tor.skelamp.se/aleohm-7/w/MemeDream/signup.php">Sign up</a>!</p>
-        </div>
-      </div>
-      <!-- /#sidebar-wrapper -->
+      <?php 
+        include 'Partials/sidebar.php';
+      ?>
 
       <!-- Page Content -->
       <div id="page-content-wrapper">
 
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-        <button class="btn btn-primary" style="margin-right: 10px;" id="menu-toggle"><i class="fas fa-caret-left" data-fa-transform="grow-12"></i></span></button>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="navbar-brand" href="index.php">MemeDream</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php">Popular <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Hot</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">New</a>
-              </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-              <button class="btn btn-outline-primary my-2 my-sm-0 create-btn" id="CreateBtn" type="button">Create</button>
-            </form>
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="http://tor.skelamp.se/aleohm-7/w/MemeDream/login.php">Login <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="http://tor.skelamp.se/aleohm-7/w/MemeDream/signup.php">Sign up <span class="sr-only">(current)</span></a>
-              </li>
-            </ul>
-          </div>
-      </nav>
+      <?php 
+          include 'Partials/navbar.php';
+      ?>
 
         <div class="container-fluid">
           
@@ -79,15 +39,6 @@
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Menu Toggle Script -->
-    <script>
-      $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-      });
-    </script>
-
  
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
