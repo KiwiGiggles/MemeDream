7fafce501e96de259efbef3d7ab0188e00af6873
Alexander Öhman <f.alexander.ohman@gmail.com>
2019-04-29 23:49:15 +0200

    Confirmed working

--- signup.php
+++ signup.php
@@ -13,9 +13,9 @@ if(isset($_SESSION['user_id'])){
 if(isset($_POST['signup'])){
   $username = trim($_POST["username"]);
   $password = trim($_POST["password"]);
+  $email = trim($_POST["email"]);
   $check_query = " SELECT * FROM user
-  WHERE username = :username
-  ";
+  WHERE username = :username";
   $statement = $connect->prepare($check_query);
   $check_data = array(
     ':username'   => $username
@@ -29,6 +29,9 @@ if(isset($_POST['signup'])){
       if(empty($username)){
         $message .= '<p><label>Username is required</label></p>';
       }
+      if(empty($email)){
+        $message .= '<p><label>email is required</label></p>';
+      }
       if(empty($password)){
         $message .= '<p><label>Password is required</label></p>';
       }
@@ -40,12 +43,13 @@ if(isset($_POST['signup'])){
       if($message == ''){
         $data = array(
           ':username'   => $username,
+          ':email'   => $email,
           ':password'   => password_hash($password, PASSWORD_DEFAULT)
         );
 
-        $query = " INSERT INTO user
-        (username, password)
-        VALUES (:username, :password)
+        $query = "INSERT INTO user
+        (username, password, email)
+        VALUES (:username, :password, :email)
         ";
 
         $statement = $connect->prepare($query);
@@ -77,16 +81,56 @@ if(isset($_POST['signup'])){
 
     <div class="d-flex" id="wrapper">
 
-      <?php 
-        include 'Partials/sidebar.php';
-      ?>
+      <!-- Sidebar -->
+      <div class="bg-dark border-right" style="border-right: 1px solid rgba(0,0,0,.125)!important;" id="sidebar-wrapper">
+        <div class="sidebar-heading" style="color: white;">Followed users</div>
+        <div class="list-group list-group-flush">
+          <p style="color: white; margin: 5px;">You need to <a href="http://tor.skelamp.se/aleohm-7/w/MemeDream/login.php">login</a> to see followed users</p>
+          <p style="color: white; margin: 5px;">Don't have an account? <a href="http://tor.skelamp.se/aleohm-7/w/MemeDream/signup.php">Sign up</a>!</p>
+        </div>
+      </div>
+      <!-- /#sidebar-wrapper -->
 
       <!-- Page Content -->
       <div id="page-content-wrapper">
 
-      <?php 
-          include 'Partials/navbar.php';
-      ?>
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+        <button class="btn btn-primary" style="margin-right: 10px;" id="menu-toggle"><i class="fas fa-caret-left" data-fa-transform="grow-12"></i></span></button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="navbar-brand" href="index.php">MemeDream</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Popular <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Hot</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">New</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+              <button class="btn btn-outline-primary my-2 my-sm-0 create-btn" id="CreateBtn" type="button">Create</button>
+            </form>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="login.php">Login <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="http://tor.skelamp.se/aleohm-7/w/MemeDream/signup.php">Sign up <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+          </div>
+      </nav>
 
         <div class="container-fluid">
             <h2 class="mt-4 mb-4">Sign up</h2>  
@@ -98,8 +142,8 @@ if(isset($_POST['signup'])){
                     <input type="text" name="username" class="form-control login-input" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter username">
                 </div>
                 <div class="form-group">
-                    <label for="InputEmail1">Email address</label>
-                    <input type="email" class="form-control login-input" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <label for="InputEmail2">Email</label>
+                    <input type="email" name="email" class="form-control login-input" id="InputEmail2" aria-describedby="emailHelp" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="InputPassword1">Password</label>
@@ -110,7 +154,7 @@ if(isset($_POST['signup'])){
                     <input type="password" name="confirm_password" class="form-control login-input" id="InputPassword2" placeholder="Password">
                 </div>
                 <div class="form-group">
-                <input type="submit" name="signup" class="btn btn-primary" value="submit" />
+                <button type="submit" name="signup" class="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
@@ -123,6 +167,15 @@ if(isset($_POST['signup'])){
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+      $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
+    </script>
+
  
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
